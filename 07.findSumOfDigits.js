d0ec7185a59106e8319a786ab7fd478814320703
AvetBadalyan <avetbadalyan@gmail.com>
2022-02-21 12:45:47 +0400

    feat: sum the digits in number with recursion

---
+++ 07.findSumOfDigits.js
@@ -0,0 +1,19 @@
+/* Given a number. Write a function that calculates its sum of the digits and if that sum has
+more than 1 digit find the sum of digits of that number. Repeat that process if needed
+and return the result. */
+
+function sumOfDigits(number) {
+  number = Math.abs(number);
+  return String(number)
+    .split("")
+    .reduce(function (aggr, digit) {
+      if (aggr >= 10) {
+        return sumOfDigits(aggr);
+      }
+      return aggr + Number(digit);
+    }, 0);
+}
+
+console.log(sumOfDigits(56)); // 5
+console.log(sumOfDigits(29)); // 2
+console.log(sumOfDigits(999)); // 9
